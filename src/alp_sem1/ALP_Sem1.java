7facf1087bd12c74083441e667be266a672ab66a
Oxqlion <88870464+oxqlion@users.noreply.github.com>
2022-12-26 08:40:38 +0700

    Add files via upload

---
+++ src/alp_sem1/ALP_Sem1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alp_sem1;
+
+/**
+ *
+ * @author SHINTA
+ */
+public class ALP_Sem1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
